3ca87dcc5216477b854885093e73720e0633cdb0
deegiimurun <deegiimurun@gmail.com>
2023-08-31 13:22:15 +0800

    latest

--- src/components/chatbox.tsx
+++ src/components/chatbox.tsx
@@ -6,6 +6,7 @@ import {useEffect, useRef, useState} from "react";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {useCollection} from "react-firebase-hooks/firestore";
 import {collection, addDoc, query, orderBy, limitToLast} from 'firebase/firestore';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Chatbox() {
     const [page, setPage] = useState(20);
@@ -19,10 +20,11 @@ export default function Chatbox() {
     );
     const [users] = useListVals<{ username: string, online: boolean }>(ref(db, 'users'));
     const [message, setMessage] = useState('');
-    const messageBoxRef = useRef<HTMLDivElement>(null);
+    const [items, setItems] = useState(Array.from({ length: 100 }));
+    const messageBoxRef = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
-        messageBoxRef.current?.children.item(messageBoxRef.current?.childNodes.length - 1)?.scrollIntoView();
+        console.log(snapshot?.docs)
     }, [snapshot]);
 
     function sendChat() {
@@ -38,25 +40,48 @@ export default function Chatbox() {
         setMessage('');
     }
 
+    function fetchMoreData() {
+        setItems(items.concat(Array.from({ length: 100 })));
+    }
+
     return (
-        <Stack spacing={2}>
-            <Stack spacing={2} ref={messageBoxRef} height={'50vh'} sx={{
-                overflowY: 'scroll',
-                overflowX: 'hidden',
-            }}>
-                {!loading && snapshot && (
-                    snapshot.docs.map((v) => {
-                        const isUserOnline = users!.find((u) => u.username === v.data().username && u.online);
-                        return (
-                            <Stack key={`${v.id}`} direction={'row'} spacing={2} alignItems={'center'}>
-                                <Typography
-                                    color={isUserOnline ? 'green' : 'red'}>{v.data().username.replaceAll('@webchat.com', '')}:</Typography>
-                                <Typography>{v.data().message}</Typography>
-                            </Stack>
-                        )
-                    })
-                )}
-            </Stack>
+        <Stack spacing={2}
+               style={{
+                   height: '50vh',
+                   overflow: 'auto',
+                   display: 'flex',
+               }}>
+            <div
+                id="scrollableDiv"
+                style={{
+                    overflow: 'auto',
+                    display: 'flex',
+                    flexDirection: 'column-reverse',
+                }}
+            >
+                <InfiniteScroll
+                    dataLength={snapshot?.size || 0}
+                    next={() => {setPage(prevState => prevState + 20)}}
+                    style={{ display: 'flex', flexDirection: 'column-reverse' }}
+                    inverse={true} //
+                    hasMore={true}
+                    loader={<h4>Loading...</h4>}
+                    scrollableTarget="scrollableDiv"
+                >
+                    {!loading && snapshot && (
+                        snapshot.docs.reverse().map((v) => {
+                            const isUserOnline = users!.find((u) => u.username === v.data().username && u.online);
+                            return (
+                                <Stack key={`${v.id}`} direction={'row'} spacing={2} alignItems={'center'}>
+                                    <Typography
+                                        color={isUserOnline ? 'green' : 'red'}>{v.data().username.replaceAll('@webchat.com', '')}:</Typography>
+                                    <Typography>{v.data().message}</Typography>
+                                </Stack>
+                            )
+                        })
+                    )}
+                </InfiniteScroll>
+            </div>
             <TextField
                 label="Message"
                 multiline
